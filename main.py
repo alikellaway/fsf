c529e28516a7bff2941baa0ca3591b5449379e66
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-05-03 12:05:47 +0100

    Cathup commit.

--- main.py
+++ main.py
@@ -15,8 +15,11 @@ if __name__ == '__main__':
     # with timer():
     #     dpus = fsf.get_duplicates("C:\\Users\\AlistairKellaway\\git")
 
-    fsf.create_test_directory(4, "test_direc")
-    print(fsf.get_duplicates("test_direc"))
+    # fsf.create_test_directory(4, "test_direc")
+    r = fsf.get_duplicates("test_direc")
+    for res in r.items():
+        print(res)
+        print()
 
 
 
