fd3ec37b43e245692d51ddb92bd0a795230bec4b
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-05-03 11:48:32 +0100

    Small tests etc. keep up commit.

--- main.py
+++ main.py
@@ -12,5 +12,13 @@ def timer():
 
 
 if __name__ == '__main__':
-    with timer():
-        fsf.get_duplicatea
+    # with timer():
+    #     dpus = fsf.get_duplicates("C:\\Users\\AlistairKellaway\\git")
+
+    fsf.create_test_directory(4, "test_direc")
+    print(fsf.get_duplicates("test_direc"))
+
+
+
+    # print(dpus)
+    # print(dpus2)
